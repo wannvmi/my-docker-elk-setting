069dc46e2602991b42c59aa903cc6ff2afcd8d67
pursue <996198546@qq.com>
2020-11-25 19:02:29 +0800

    add sender demo

---
+++ demo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using Serilog;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt",
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true)
+                .Enrich.WithProperty("target_index",
+                    $"serilog-{Assembly.GetExecutingAssembly().GetName().Name}"
+                        .ToLower())
+                .WriteTo.Http("http://192.168.16.152:8087")
+                .CreateLogger();
+
+            Log.Information("Hello, Serilog!");
+
+            Log.CloseAndFlush();
+
+        }
+    }
+}
